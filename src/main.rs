8fd464ac176b2f09ab080f953e3a3d9556d956d9
monthebrice2000 <monthedjeumoubrice2000@gmail.com>
2022-02-14 17:43:59 +0100

    add Home , Login and Register Components

--- src/main.rs
+++ src/main.rs
@@ -7,61 +7,327 @@ fn main() {
 }
 
 fn app(cx: Scope) -> Element {
+    cx.render( rsx!{
+        Home{
+            score : 3,
+        }
+    })
+}
+
+#[derive(PartialEq, Props)]
+struct RegisterProps {
+    score: i32
+}
+
+fn Register(cx: Scope<RegisterProps>) -> Element {
     cx.render(rsx!{
-        div {
-            "class" : "main_1",
-            div{
-                class: "header",
-                div{
-                    class: "header_top",
-                    div{
-                        class: "logo",
-                        "Conduit"
-                    }
-                    div{
-                        class: "nav",
-                        span{
-                            class: "link",
-                            "Home"
-                        }
-                        span{
-                            class: "link",
-                            "Sing In"
-                        }
-                        span{
-                            class: "link",
-                            "Sign Up"
-                        }
-                    }
+        div{
+            class : "main",
+            div {
+                class : "header",
+                a{
+                    "href" : "#",
+                    class : "title",
+                    "Conduit"
                 }
-                div{
-                    class: "header_bottom",
-                    h2 {
-                        "Conduit"
+                div {
+                    class : "nav",
+                    a{
+                        "href" : "#",
+                        class : "item active",
+                        "Home"
                     }
-                    p{
-                        "A Place To Share Your Kwonledge"
+                    a{
+                        "href" : "#",
+                        class : "item",
+                        "Sign in"
+                    }
+                    a{
+                        "href" : "#",
+                        class : "item",
+                        "Sign up"
                     }
                 }
             },
-            div{
-                class: "main_2",
-                "MAIN"
-            },
-            div{
-                "class" : "footer",
-                span{
+            div {
+                class : "body",
+                h2{
+                    class : "item title",
+                    "Sign up"
+                }
+                a{
+                    class : "item login",
+                    href : "#",
+                    "Have an account?"
+                }
+                form {
+                    class : "item form",
+                    method : "post",
+                    action : "#",
+                    name : "form",
+                    input{
+                        "type" : "text",
+                        class : "input username",
+                        name : "username",
+                        placeholder : "Username"
+                    }
+                    input{
+                        "type" : "email",
+                        class : "input email",
+                        name : "email",
+                        placeholder : "Email"
+                    }
+                    input{
+                        "type" : "submit",
+                        class : "input submit",
+                        name : "submit",
+                        value : "Sign up"
+                    }
+                }
+            }
+            a {
+                class : "footer",
+                "href" : "https://github.com/gothinkster/angularjs-realworld-example-app",
+                target : "_blank",
+                span {
                     class : "logo",
-                    img{
-                        src: "img/github.png",
+                    i{
+                        class : "fab fa-github",
                     }
                 }
                 span{
                     class : "title",
                     "Fork On Github"
                 }
-            }
-        }
 
+            }
+
+        }
+    })
+}
+
+#[derive(PartialEq, Props)]
+struct HomeProps {
+    score: i32
+}
+
+fn Home(cx: Scope<HomeProps>) -> Element {
+    cx.render(rsx!{
+        div{
+            class : "main home",
+            div {
+                class : "header",
+                a{
+                    "href" : "#",
+                    class : "title",
+                    "Conduit"
+                }
+                div {
+                    class : "nav",
+                    a{
+                        "href" : "#",
+                        class : "item active",
+                        "Home"
+                    }
+                    a{
+                        "href" : "#",
+                        class : "item",
+                        "Sign in"
+                    }
+                    a{
+                        "href" : "#",
+                        class : "item",
+                        "Sign up"
+                    }
+                }
+            },
+            div {
+                class : "header_description",
+                div {
+                    class : "title",
+                    "conduit"
+                }
+                div{
+                    class : "description",
+                    "A place to share your knowledge"
+                }
+            }
+            div {
+                class : "body",
+                div {
+                    class : "feeds",
+                    div{
+                        class : "nav",
+                        div {
+                            class : "item first",
+                            "YOUR FEEDS"
+                        }
+                        div {
+                            class : "item second active",
+                            "GLOBAL FEEDS"
+                        }
+                        div {
+                            class : "item third",
+                            "ANOTHER SEARCH"
+                        }
+                    }
+                    div {
+                        class : "content second",
+                        div {
+                            class : "item_ first",
+                            "ALL YOUR FEEDS"
+                        }
+                        div {
+                            class : "item_ second",
+                            "ALL GLOBAL FEEDS"
+                        }
+                        div {
+                            class : "item_ third",
+                            "ALL ANOTHER SEARCH"
+                        }
+                    }
+                }
+                div {
+                    class : "aside",
+                    div{
+                        class : "title",
+                        "Popular Tags"
+                    }
+                    a{
+                        class : "title",
+                        "href" : "#",
+                        class : "tag",
+                        "welcome"
+                    }
+                    a{
+                        class : "title",
+                        "href" : "#",
+                        class : "tag",
+                        "implementations"
+                    }
+                    a{
+                        class : "title",
+                        "href" : "#",
+                        class : "tag",
+                        "codebaseShow"
+                    }
+                    a{
+                        class : "title",
+                        "href" : "#",
+                        class : "tag",
+                        "introduction"
+                    }
+                }
+            }
+            a {
+                class : "footer",
+                "href" : "https://github.com/gothinkster/angularjs-realworld-example-app",
+                target : "_blank",
+                span {
+                    class : "logo",
+                    i{
+                        class : "fab fa-github",
+                    }
+                }
+                span{
+                    class : "title",
+                    "Fork On Github"
+                }
+
+            }
+
+        }
+    })
+}
+
+#[derive(PartialEq, Props)]
+struct LoginProps {
+    score: i32
+}
+
+fn Login(cx: Scope<LoginProps>) -> Element {
+    cx.render(rsx!{
+        div{
+            class : "main",
+            div {
+                class : "header",
+                a{
+                    "href" : "#",
+                    class : "title",
+                    "Conduit"
+                }
+                div {
+                    class : "nav",
+                    a{
+                        "href" : "#",
+                        class : "item",
+                        "Home"
+                    }
+                    a{
+                        "href" : "#",
+                        class : "item active",
+                        "Sign in"
+                    }
+                    a{
+                        "href" : "#",
+                        class : "item",
+                        "Sign up"
+                    }
+                }
+            },
+            div {
+                class : "body",
+                h2{
+                    class : "item title",
+                    "Sign in"
+                }
+                a{
+                    class : "item register",
+                    href : "#",
+                    "Need An Account?"
+                }
+                form {
+                    class : "item form",
+                    method : "post",
+                    action : "#",
+                    name : "form",
+                    input{
+                        "type" : "email",
+                        class : "input email",
+                        name : "email",
+                        placeholder : "Email"
+                    }
+                    input{
+                        "type" : "password",
+                        class : "input password",
+                        name : "password",
+                        placeholder : "Password"
+                    }
+                    input{
+                        "type" : "submit",
+                        class : "input submit",
+                        name : "submit",
+                        value : "Sign up"
+                    }
+                }
+            }
+            a {
+                class : "footer",
+                "href" : "https://github.com/gothinkster/angularjs-realworld-example-app",
+                target : "_blank",
+                span {
+                    class : "logo",
+                    i{
+                        class : "fab fa-github",
+                    }
+                }
+                span{
+                    class : "title",
+                    "Fork On Github"
+                }
+
+            }
+
+        }
     })
 }
